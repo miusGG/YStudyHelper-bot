ec26e2c54e0f6c8c59a4eb6d534f5e61d6a4e245
Egor <romanovegorsergeevich@gmail.com>
2024-03-15 15:50:26 +0300

    + создание объявления для учителя + структура docs

--- main.py
+++ main.py
@@ -9,6 +9,63 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='>', intents=intents)
 auth_channel1 = 1206656465616764998
 guild_id = 1206656352857096312
+news1_id = 1218168029288861757
+news2_id = 1218168055386083369
+#не забыть заменить id на нужные!!!!!!!!!!!!!
+
+class HelperView(discord.ui.View):
+    @discord.ui.button(label="Как задать вопрос учителю?", row=0, style=discord.ButtonStyle.primary)
+    async def first_button_callback(self, interaction, button):
+        await interaction.response.send_message("В специальном тексковом канале пишите команду - >question")
+
+    @discord.ui.button(label="Все команды", row=1, style=discord.ButtonStyle.primary)
+    async def second_button_callback(self, interaction, button):
+        await interaction.response.send_message("Вот все доступные команды:\n>question - создать канал для вопроса учителю.\n>? - команда по выдачи документаций и полезных материалов для учеников!")
+
+
+class DocsView3(discord.ui.View):
+    @discord.ui.button(label="1 проект", row=0, style=discord.ButtonStyle.primary)
+    async def firstProject_button_callback(self, interaction, button):
+        pass
+
+    @discord.ui.button(label="2ой проект", row=1, style=discord.ButtonStyle.primary)
+    async def secondProject_button_callback(self, interaction, button):
+        pass
+
+    @discord.ui.button(label="3", row=0, style=discord.ButtonStyle.primary)
+    async def thirdProject_button_callback(self, interaction, button):
+        pass
+
+    @discord.ui.button(label="4", row=1, style=discord.ButtonStyle.primary)
+    async def fourthProject_button_callback(self, interaction, button):
+        pass
+
+
+class DocsView2(discord.ui.View):
+    @discord.ui.button(label="1 тема", row=0, style=discord.ButtonStyle.primary)
+    async def firstModule_button_callback(self, interaction, button):
+        await interaction.response.send_message("Иди сам разбирайся")
+
+    @discord.ui.button(label="2ой тема", row=1, style=discord.ButtonStyle.primary)
+    async def secondModule_button_callback(self, interaction, button):
+        pass
+
+    @discord.ui.button(label="3", row=0, style=discord.ButtonStyle.primary)
+    async def thirdModule_button_callback(self, interaction, button):
+        pass
+
+    @discord.ui.button(label="4", row=1, style=discord.ButtonStyle.primary)
+    async def fourthModule_button_callback(self, interaction, button):
+        pass
+
+class DocsView1(discord.ui.View):
+    @discord.ui.button(label="1 ГОД обучения", row=0, style=discord.ButtonStyle.primary, emoji="🐸")
+    async def firstYear_button_callback(self, interaction, button):
+        await interaction.response.send_message("Какой модуль в 1ом году вас интересует?", view=DocsView2())
+
+    @discord.ui.button(label="2 ГОД обучения", row=1, style=discord.ButtonStyle.primary, emoji="🐘")
+    async def secondYear_button_callback(self, interaction, button):
+        await interaction.response.send_message("Какая тема вас инетерисует в 2ом году?", view=DocsView3())
 
 
 @bot.command()
@@ -32,18 +89,6 @@ async def question(ctx):
     await ctx.message.delete()
 
 
-
-
-class HelperView(discord.ui.View):
-    @discord.ui.button(label="Как задать вопрос учителю?", row=0, style=discord.ButtonStyle.primary)
-    async def first_button_callback(self, interaction, button):
-        await interaction.response.send_message("В специальном тексковом канале пишите команду - >question")
-
-    @discord.ui.button(label="Все команды", row=1, style=discord.ButtonStyle.primary)
-    async def second_button_callback(self, interaction, button):
-        await interaction.response.send_message("Вот все доступные команды:\n>question - создать канал для вопроса учителю.\n>? - команда по выдачи документаций и полезных материалов для учеников!")
-
-
 @bot.event
 async def on_member_join(member):
     role = member.guild.get_role(role_id=1208730162414485555)
@@ -51,6 +96,7 @@ async def on_member_join(member):
 
 
 @bot.command()
+@commands.has_role("new")
 async def make_channel(ctx):
     user = ctx.author
     guild = ctx.guild
@@ -84,7 +130,7 @@ async def make_channel(ctx):
         role1 = discord.utils.get(ctx.guild.roles, name='группа 2')
     else:
         await message1.delete()
-        await channel.send('Неверный выбор.')
+        await channel.send('Ошибка... Вы что-то не так выбрали..')
         return
 
     await message1.delete()
@@ -106,7 +152,7 @@ async def make_channel(ctx):
         role = discord.utils.get(ctx.guild.roles, name='год 2')
     else:
         await message.delete()
-        await channel.send('Неверный выбор.')
+        await channel.send('Ошибка... Вы что-то не так выбрали.')
         return
 
     await message.delete()
@@ -146,7 +192,6 @@ async def make_channel(ctx):
         await ctx.send("You did not react in time.")
 
 
-
 @bot.command()
 async def delete_channel(ctx):
     def check(msg):
@@ -169,6 +214,63 @@ async def helper(ctx):
     member = ctx.author
     message_content = f'Руководство по использованию этого бота.\nВот все доступные команды:\n>question - создать канал для вопроса учителю.\n>? - команда по выдачи документаций и полезных материалов для учеников!\nВыберите что бы Вы хотели бы узнать:'
     await user.send(message_content, view=HelperView())
+
+
+@bot.command()
+@commands.has_role("Admin")
+async def news(ctx):
+    user = ctx.author
+    guild = ctx.guild
+    member = user
+    channel = ctx.channel
+    await ctx.message.delete()
+
+    name_msg = member
+
+    message1 = await channel.send('Выберите группу, которой написать объявление:\n1. Группа 1\n2. Группа 2')
+
+    await message1.add_reaction('1️⃣')
+    await message1.add_reaction('2️⃣')
+
+    def check(reaction, user):
+        return user == ctx.author and str(reaction.emoji) in ['1️⃣', '2️⃣']
+
+    reaction, _ = await bot.wait_for('reaction_add', timeout=60.0, check=check)
+
+    if reaction.emoji == '1️⃣':
+        team1 = True
+    elif reaction.emoji == '2️⃣':
+        team1 = False
+    else:
+        await message1.delete()
+        await channel.send('Ошибка... Вы что-то не так выбрали.')
+        return
+
+    await message1.delete()
+    await channel.send('Напишите объявление для этой группы:')
+    announcement = await bot.wait_for('message', check=lambda m: m.author == ctx.author)
+
+    if team1:
+        global news1_id
+        news_channel = guild.get_channel(news1_id)
+        await news_channel.send(announcement.content)
+        await ctx.send("Объявление успешно переслано.")
+
+    else:
+        global news2_id
+        news_channel = guild.get_channel(news2_id)
+        await news_channel.send(announcement.content)
+        await ctx.send("Объявление успешно переслано.")
+
+
+@bot.command()
+async def docs(ctx):
+    user = ctx.message.author
+    message_content = f'Вспомогательня информация по всей программе за 2 года обучения. Выберите свой год обучения!'
+    await user.send(message_content, view=DocsView1())
+    message = ctx.message
+    await message.delete()
+
 
 
 bot.run(token)
