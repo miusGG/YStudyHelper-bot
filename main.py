7eb61e1b4bd407686e7a0c3a0403d73a14449389
Egor <romanovegorsergeevich@gmail.com>
2024-04-14 15:34:14 +0300

    Создание докуметации для админов. Исправление spelling mistakes. Доьбавление и улучшения функций news для админов. Видео инсталл питона. Авто удаление роли new. Перенос классов в файл lasses. Доабвдения кнопки НАЗАД в >docs.

--- main.py
+++ main.py
@@ -2,69 +2,22 @@ import discord
 from discord.ext import commands
 from info import token
 from discord.utils import get
-from discord import ui, app_commands
-import datetime
 import asyncio
-import os
+from classes import HelperView, DocsView1
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='>', intents=intents)
 auth_channel1 = 1206656465616764998
 guild_id = 1206656352857096312
-news1_id = 1218168029288861757
-news2_id = 1218168055386083369
+news11_id = 1218168029288861757
+news12_id = 1218168055386083369
+news21_id = 1
+news22_id = 1
+bogdan_id = 416583789255327745
 #не забыть заменить id на нужные!!!!!!!!!!!!!
 
-class HelperView(discord.ui.View):
-    @discord.ui.button(label="Как задать вопрос учителю?", row=0, style=discord.ButtonStyle.primary)
-    async def first_button_callback(self, interaction, button):
-        await interaction.response.send_message("В специальном тексковом канале пишите команду - >question")
 
-    @discord.ui.button(label="Все команды", row=1, style=discord.ButtonStyle.primary)
-    async def second_button_callback(self, interaction, button):
-        await interaction.response.send_message("Вот все доступные команды:\n>question - создать канал для вопроса учителю.\n>docs - команда по выдачи документаций и полезных материалов для учеников!")
-
-
-class DocsView3(discord.ui.View):
-    @discord.ui.button(label="PyQt5", row=0, style=discord.ButtonStyle.primary)
-    async def firstProject_button_callback(self, interaction, button):
-        await interaction.response.send_message("Официальная документация - https://doc.qt.io/qtforpython-5/contents.html \nУдобное краткое руководство - https://translated.turbopages.org/proxy_u/en-ru.ru.2ace4ec1-660bcb43-5c3e9812-74722d776562/https/www.tutorialspoint.com/pyqt5/pyqt5_quick_guide.htm \nВидео про кнопки на англ. - https://www.youtube.com/watch?time_continue=423&v=-2uyzAqefyE&embeds_referring_euri=https%3A%2F%2Fyastatic.net%2Fvideo-player%2F0xe7891f6f377%2Fpages-common%2Fyoutube%2Fyoutube.html&source_ve_path=Mjg2NjY&feature=emb_logo \nВидео про простейший калькултор на PyQt5 - https://www.youtube.com/watch?time_continue=292&v=7aAF0s7-5io&embeds_referring_euri=https%3A%2F%2Fyastatic.net%2Fvideo-player%2F0xe7891f6f377%2Fpages-common%2Fyoutube%2Fyoutube.html&source_ve_path=Mjg2NjY&feature=emb_logo \n Туториал по работе с PyQt5 и QtDesigner (Создание гениратора паролей) -  https://www.youtube.com/watch?time_continue=454&v=pMNrxE1xAfw&embeds_referring_euri=https%3A%2F%2Fyastatic.net%2Fvideo-player%2F0xe7891f6f377%2Fpages-common%2Fyoutube%2Fyoutube.html&source_ve_path=Mjg2NjY&feature=emb_logo \nКоманды для import библеотек: \n```pip install PyQt5Designer```\n```pip install PyQt5```", view=DocsView3())
-
-    @discord.ui.button(label="SQL Базы данных", row=1, style=discord.ButtonStyle.primary)
-    async def secondProject_button_callback(self, interaction, button):
-        await interaction.response.send_message("Скачать SQLiteStudio - https://sqlitestudio.pl/ \nSQLiteStudio как пользоваться - https://www.youtube.com/watch?v=QH_wPA0ojRc \nБольшая практическая шпаргалка SQL (SQLite) с готовыми запросами - https://habr.com/ru/articles/792630/ \nКомнада для добавления бибилеотки: ```pip install pymysql```", view=DocsView3())
-
-    @discord.ui.button(label="PyGame", row=0, style=discord.ButtonStyle.primary)
-    async def thirdProject_button_callback(self, interaction, button):
-        await interaction.response.send_message("Официальная документация - https://pygame-docs.website.yandexcloud.net/ref/pygame.html \nВидео как сделать игру на весь экран - https://www.youtube.com/watch?v=wP7PJohQOMc \nВидео как сделать меню - https://www.youtube.com/watch?v=kqrlD8X8pdw \nШпаргалка - https://waksoft.susu.ru/2019/04/24/pygame-shpargalka-dlja-ispolzovanija/ \nКоманда для import - ```pip install pygame```", view=DocsView3())
-
-    @discord.ui.button(label="Flusk", row=1, style=discord.ButtonStyle.primary)
-    async def fourthProject_button_callback(self, interaction, button):
-        await interaction.response.send_message("Официальная документация - https://flask.palletsprojects.com/en/3.0.x/ \nFlusk шпаргалка(англ) - https://codeinsightacademy.com/blog/python/flask-cheat-sheet/ \nFlask работа с базой данных - https://www.youtube.com/watch?v=_CwCk5g8YJg&t=2s \nКоманда для import -```pip install Flask```", view=DocsView3())
-
-    @discord.ui.button(label="Остальные", row=1, style=discord.ButtonStyle.primary)
-    async def fiveProject_button_callback(self, interaction, button):
-        await interaction.response.send_message("Официальная документация по телеграм боту - https://docs.python-telegram-bot.org/en/stable/index.html \nОфициальная документация по дискорд боту - https://discordpy.readthedocs.io/en/stable/index.html \nКомнады для import:\n```pip install discord.py```\n```pip install pyTelegramBotAPI```", view=DocsView3())
-
-
-class DocsView2(discord.ui.View):
-    @discord.ui.button(label="Скачивание IDE и настройка", row=0, style=discord.ButtonStyle.primary)
-    async def firstModule_button_callback(self, interaction, button):
-        await interaction.response.send_message("Видео итструкция (не официальная) - https://yandex.ru/video/preview/12460819480399738578 \nСсылка на скачивание с сайта разработчиков - https://www.jetbrains.com/pycharm-edu/ \n", view=DocsView2())
-
-    @discord.ui.button(label="Вспомогательная информация при ихучении Python", row=1, style=discord.ButtonStyle.primary)
-    async def secondModule_button_callback(self, interaction, button):
-        await interaction.response.send_message("Вспомогательный курс по Python - https://itproger.com/course/python \n Удобная документация - https://www.python.org/doc/ \n Комнадна для обновления pip - ``` python -m pip install --upgrade pip```", view=DocsView2())
-
-class DocsView1(discord.ui.View):
-    @discord.ui.button(label="1 ГОД обучения", row=0, style=discord.ButtonStyle.primary, emoji="🤓")
-    async def firstYear_button_callback(self, interaction, button):
-        await interaction.response.send_message("Какой модуль в 1ом году вас интересует?", view=DocsView2())
-
-    @discord.ui.button(label="2 ГОД обучения", row=1, style=discord.ButtonStyle.primary, emoji="😎")
-    async def secondYear_button_callback(self, interaction, button):
-        await interaction.response.send_message("Какая тема вас инетерисует в 2ом году?", view=DocsView3())
 
 
 @bot.command()
@@ -90,7 +43,9 @@ async def question(ctx):
         admin_role: discord.PermissionOverwrite(read_messages=True)
     }
     channel = await guild.create_text_channel(f'Вопрос от {member}', overwrites=overwrites)
-    await channel.send(f"@{user} хочет задать вопрос! @wander1ust_")
+    global bogdan_id
+    bogdan = user = await bot.fetch_user(bogdan_id)
+    await channel.send(f"{user} хочет задать вопрос! {bogdan.mention}")
     await ctx.message.delete()
 
 
@@ -188,6 +143,8 @@ async def make_channel(ctx, name_msg1, name_msg2, name_msg3):
 
             await ctx.author.add_roles(role)
             await ctx.author.add_roles(role1)
+            new_role = discord.utils.get(ctx.guild.roles, id=1208730162414485555, name="new")
+            await ctx.author.remove_roles(new_role)
             await channel.send('Выбор подтвержден.')
             await channel.delete()
         elif str(reaction.emoji) == '❌':
@@ -202,6 +159,7 @@ async def make_channel(ctx, name_msg1, name_msg2, name_msg3):
 
 
 @bot.command()
+@commands.has_role("Admin")
 async def delete_channel(ctx):
     def check(msg):
         return msg.author == ctx.author and msg.channel == ctx.channel and msg.content.lower() in ['да', 'нет']
@@ -227,49 +185,84 @@ async def helper(ctx):
 
 @bot.command()
 @commands.has_role("Admin")
+async def admin_helper(ctx):
+    await ctx.message.delete()
+    user = ctx.message.author
+    member = ctx.author
+    message_content = ">news - для создания новости\n>delete_channel - удалить канал"
+    await user.send(message_content, view=HelperView())
+
+
+@bot.command()
+@commands.has_role("Admin")
 async def news(ctx):
     user = ctx.author
     guild = ctx.guild
     member = user
     channel = ctx.channel
     await ctx.message.delete()
-
     name_msg = member
 
-    message1 = await channel.send('Выберите группу, которой написать объявление:\n1. Группа 1\n2. Группа 2')
+    message1 = await channel.send('Выберите группу, которой написать объявление:\n1️⃣ - 1 группа 1 год\n2️⃣ - 2 группа 1 год\n3️⃣ - 1 группа 2 год\n4️⃣ - 2 группа 2 год\n0️⃣ - ВСЕМ')
 
     await message1.add_reaction('1️⃣')
     await message1.add_reaction('2️⃣')
+    await message1.add_reaction('3️⃣')
+    await message1.add_reaction('4️⃣')
+    await message1.add_reaction('0️⃣')
 
     def check(reaction, user):
-        return user == ctx.author and str(reaction.emoji) in ['1️⃣', '2️⃣']
+        return user == ctx.author and str(reaction.emoji) in ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '0️⃣']
 
     reaction, _ = await bot.wait_for('reaction_add', timeout=60.0, check=check)
 
     if reaction.emoji == '1️⃣':
-        team1 = True
+        team11 = True
     elif reaction.emoji == '2️⃣':
-        team1 = False
+        team12 = True
+    elif reaction.emoji == '3️⃣':
+        team21 = True
+    elif reaction.emoji == '4️⃣':
+        team22 = True
+    elif reaction.emoji == '0️⃣':
+        team11 = True
+        team12 = True
+        team21 = True
+        team22 = True
     else:
         await message1.delete()
         await channel.send('Ошибка... Вы что-то не так выбрали.')
         return
 
+
+
     await message1.delete()
     await channel.send('Напишите объявление для этой группы:')
     announcement = await bot.wait_for('message', check=lambda m: m.author == ctx.author)
 
-    if team1:
-        global news1_id
-        news_channel = guild.get_channel(news1_id)
+    if team11:
+        global news11_id
+        news_channel = guild.get_channel(news11_id)
         await news_channel.send(announcement.content)
-        await ctx.send("Объявление успешно переслано.")
+        await ctx.send("Объявление успешно переслано. 1 год 1 группа")
 
-    else:
-        global news2_id
-        news_channel = guild.get_channel(news2_id)
+    if team12:
+        global news12_id
+        news_channel = guild.get_channel(news12_id)
         await news_channel.send(announcement.content)
-        await ctx.send("Объявление успешно переслано.")
+        await ctx.send("Объявление успешно переслано. 1 год 2 группа")
+
+    if team21:
+        global news21_id
+        news_channel = guild.get_channel(news21_id)
+        await news_channel.send(announcement.content)
+        await ctx.send("Объявление успешно переслано. 2 год 1 группа")
+
+    if team22:
+        global news22_id
+        news_channel = guild.get_channel(news22_id)
+        await news_channel.send(announcement.content)
+        await ctx.send("Объявление успешно переслано. 2  год 2 группаs")
 
 
 @bot.command()
@@ -281,5 +274,4 @@ async def docs(ctx):
     await message.delete()
 
 
-#bot.run(token, bot=True)
-bot.run("MTIwNjY1NzE4MjEzNzg1MTk5NA.G-QwzL.fYiWP2cgJsOdM-_jE1_4DYLGt468YQdrRc4urY")
+bot.run(token)
